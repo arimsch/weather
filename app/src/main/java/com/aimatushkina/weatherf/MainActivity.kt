7df378479efc745d290b2d780aa30bf764a74823
arimsch <arina1998-v2013@yandex.ru>
2021-11-01 20:26:31 +0300

    first com

---
+++ app/src/main/java/com/aimatushkina/weatherf/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aimatushkina.weatherf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
